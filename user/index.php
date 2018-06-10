3db25e867372141e4eddb4041f380df3f493f50b
Timothy Boss <bosst@wit.edu>
2018-06-10 15:55:55 -0400

    update

--- user/index.php
+++ user/index.php
@@ -52,7 +52,7 @@ mysqli_close($link);
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto text-right">
+      <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
             aria-expanded="false">
