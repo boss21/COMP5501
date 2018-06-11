fe71975166dfd535a797bf396f0f12e5bfab8ae2
Timothy Boss <bosst@wit.edu>
2018-06-11 14:39:45 -0400

    update

--- user/months/june.php
+++ user/months/june.php
@@ -30,7 +30,22 @@ while ($row = mysqli_fetch_array($result)){
     $itemNames[$row['day']] = $row['itemName'];
     $itemNames[$row['day']] = $row['itemAmount'];
 }
-    
 mysqli_free_result($result);
 mysqli_close($link);
+
+for ($i = 0; $i < 7; $i++){
+    echo "<h4>WEEK 1</h4>";
+}
+for ($i = 7; $i < 14; $i++){
+    echo "<h4>WEEK 2</h4>";
+}
+for ($i = 14; $i < 21; $i++){
+    echo "<h4>WEEK 3</h4>";
+}
+for ($i = 21; $i < 28; $i++){
+    echo "<h4>WEEK 4</h4>";
+}
+for ($i = 28; $i < 30; $i++){
+    echo "<h4>WEEK 5</h4>";
+}
 ?>
