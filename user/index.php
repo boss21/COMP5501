b9a15c73bf3d36849f044c12e5f1715edfe2c103
Timothy Boss <bosst@wit.edu>
2018-06-13 22:04:40 -0400

    update

--- user/index.php
+++ user/index.php
@@ -277,6 +277,8 @@ mysqli_close($link);
     function disableDate() {
       if (document.getElementById("frequency").value == "weekly" || document.getElementById("frequency").value == "monthly") {
         document.getElementById("itemMonthDay").disabled = true;
+      } else {
+        document.getElementById("itemMonthDay").disabled = false;
       }
     }
     function januaryView() {
