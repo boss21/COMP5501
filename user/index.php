e0e5e4ec95a58e79bc53b647f1f552dcbecdc57d
Timothy Boss <bosst@wit.edu>
2018-06-13 23:16:41 -0400

    update

--- user/index.php
+++ user/index.php
@@ -88,7 +88,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 }
 
 // Attempt select query execution
-$sql = "UPDATE users SET januaryBalance = '0', februaryBalance = '0', marchBalance = '0', aprilBalance = '0', mayBalance = '0', juneBalance = '0', julyBalance = '0', augustBalance = '0', septemberBalance = '0', octoberBalance = '0', novemberBalance = '0', decemberBalance = '0' WHERE email = '$email'";
+$sql = "UPDATE users SET januaryBalance = '', februaryBalance = '', marchBalance = '', aprilBalance = '', mayBalance = '', juneBalance = '', julyBalance = '', augustBalance = '', septemberBalance = '', octoberBalance = '', novemberBalance = '', decemberBalance = '' WHERE email = '$email'";
 mysqli_query($link, $sql);
 
 // Attempt select query execution
