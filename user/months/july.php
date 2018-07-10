09dfbc0d28f0431b1a700e7dc83b454ef8166093
Timothy Boss <bosst@wit.edu>
2018-07-09 22:50:37 -0400

    update

--- user/months/july.php
+++ user/months/july.php
@@ -45,10 +45,6 @@ if (date("m") == 7){
 // Free result set
 mysqli_free_result($result);
 
-// Attempt select query execution
-$sql = "UPDATE users SET julyBalance = '$julyBal' WHERE email = '$email'";
-mysqli_query($link, $sql);
-
 $sql = "SELECT day, itemName, itemAmount FROM july WHERE email = '$email' ORDER BY day ASC, itemAmount DESC";
 $result = mysqli_query($link, $sql);
 
@@ -196,6 +192,25 @@ if (mysqli_num_rows($result) > 0){
 
     // Attempt select query execution
     $sql = "UPDATE users SET julyBalance = '$week5' WHERE email = '$email'";
+    mysqli_query($link, $sql);
+} else {
+    // Attempt select query execution
+    $sql = "SELECT currentBalance, juneBalance FROM users WHERE email = '$email'";
+    $result = mysqli_query($link, $sql);
+    $row = mysqli_fetch_array($result);
+
+    //grab data here
+    if (date("m") == 7){
+        $julyBal = $row['currentBalance'];
+    }else{
+        $julyBal = $row['juneBalance'];
+    }
+
+    // Free result set
+    mysqli_free_result($result);
+
+    // Attempt select query execution
+    $sql = "UPDATE users SET julyBalance = '$julyBal' WHERE email = '$email'";
     mysqli_query($link, $sql);
 }
 
